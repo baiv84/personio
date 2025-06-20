cd5dae7b217be70905f35b816add8ee63a83062d
baiv84 <iambabintsev@gmail.com>
2025-06-20 15:42:55 +0700

    Fix small issues in Create handler

--- handler/create.go
+++ handler/create.go
@@ -10,14 +10,18 @@ import (
 func create(pgConn *gorm.DB, w http.ResponseWriter, r *http.Request) {
 	var human model.Person
 
-	err_json := json.NewDecoder(r.Body).Decode(&human)
-	if err_json != nil {
+	errJson := json.NewDecoder(r.Body).Decode(&human)
+	if errJson != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err_json.Error()))
+		_, err := w.Write([]byte(errJson.Error()))
+		if err != nil {
+			return
+		}
 		return
 	}
 	result := pgConn.Create(&human)
 	if result.Error != nil {
 		panic("Database connection error...")
 	}
+
 }
